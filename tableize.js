03772d57d2953ed25ee099e5756dabd523cf4dad
bbouley <b.bouley@hotmail.co.uk>
2015-08-21 15:23:02 -0600

    outputs min column size for each colum

--- tableize.js
+++ tableize.js
@@ -1,3 +1,39 @@
 var data = require('./strings');
 var people = data.mvp.people;
 var companies = data.mvp.companies;
+
+
+
+function splitToLines(string){
+  var output = string.split('\n');
+}
+
+var peopleLines =  splitToLines(people);
+
+function sortByLines(array){
+  var eachWord = [];
+  for (var i = 0; i < array.length; i++) {
+    var newArr = array[i].split(',');
+    eachWord.push(newArr);
+  }
+  return eachWord;
+}
+
+var peopleLinesArray = sortByLines(peopleLines);
+
+
+function columnSize(array){
+  lengthArray = [];
+  for (var i = 0; i < array[i].length; i++) {
+    length = 0;
+    for (var j = 0; j < array.length; j++) {
+      if(array[j][i].length > length ){
+        length = array[j][i].length;
+      }
+    }
+   lengthArray.push(length);
+  }
+ return lengthArray;
+}
+
+console.log(columnSize(linesArray));
